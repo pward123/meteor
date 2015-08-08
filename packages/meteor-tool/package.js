7134e687389a29d03386e297f063f444ea43c169
Paul Ward <pward@diem.com>
2015-08-08 13:18:39 -0500

    Fixed issue where http npm urls were broken

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'pward123:meteor-tool',
   summary: "The Meteor command-line tool with support for git+sha npm require",
-  version: '1.1.4_2',
-  git: 'https://github.com/pward123/meteor.git',
+  version: '1.1.4_3',
+  git: 'https://github.com/pward123/meteor/tree/npm-git-ssh',
   documentation: 'README.md'
 });
 
